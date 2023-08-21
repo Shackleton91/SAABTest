a83ba3a34fd137d108c4c4685975ad601d8bbb50
Shackleton91 <65281838+Shackleton91@users.noreply.github.com>
2023-08-21 13:02:13 +0100

    Create InvalidTicketException.cs

---
+++ InvalidTicketException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TicketManagementSystem
+{
+    public class InvalidTicketException : Exception
+    {
+        public InvalidTicketException(string message)
+            : base(message)
+        {
+        }
+    }
+} 
